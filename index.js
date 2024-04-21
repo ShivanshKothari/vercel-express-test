e8dfa95df3be5db2ae3e0dcf885346a47a376775
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-04-21 17:24:31 +0530

    Added event route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import indexRouter from './routes/index.js';
-
+import eventRouter from './routes/events.js';
+import cors from 'cors';
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use('/', indexRouter);
+app.use('/api/', eventRouter);
 
 app.listen(port);
